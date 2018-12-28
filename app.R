8b8cf59f98ac1ab99246a6ce5bb5ed7903b8f940
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2018-12-28 11:33:17 +0200

    Remove obsolete code

--- app.R
+++ app.R
@@ -330,44 +330,6 @@ server <- function(input, output, session) {
         
         return(hc)
     })
-    
-    output$plot1 <- renderPlot({
-        
-        sp_current <- sp_data %>% 
-            dplyr::filter(Sci_name == input$selector)
-        
-        sp_name <- sp_current$Sci_name
-        
-        obs_current <- dat %>% 
-            dplyr::filter(sp == sp_current$Species_Abb)
-        
-        # Make a subselectiong of the data containing two different epochs:
-        # 1979-1999 and 2009-
-        epochs <- obs_current %>% 
-            dplyr::select(day, begin, end) %>% 
-            tidyr::gather(variable, value, -day)
-        
-        
-        p1 <- ggplot(obs_current, aes(x = day, y = paik)) +
-            geom_line() + xlab("Day of Year") + ylab("Yks./pvm - Ind./day") +
-            ggtitle(paste0(sp_name, ", Paikalliset / Lokal / Locals")) +
-            scale_x_date(labels = date_format('%e %b')) + theme_bw()
-        
-        p2 <- ggplot(obs_current, aes(x = day, y = muutto)) +
-            geom_line() + xlab("Day of Year") + ylab("Yks./pvm - Ind./day") +
-            ggtitle(paste0(sp_name, ", Muuttavat / Flyttande / Migrants")) +
-            scale_x_date(labels = date_format('%e %b')) + theme_bw()
-        
-        p3 <- ggplot(epochs, aes(x = day, y = value, color = variable)) +
-            geom_line() + xlab("Day of Year") + ylab("Yks./pvm - Ind./day") +
-            ggtitle("Muutos / Förändring / Change") +
-            scale_color_manual(values = c("red", "blue"),
-                               labels = c("1979-1999", "2009-")) +
-            scale_x_date(labels = date_format('%e %b')) +
-            theme_bw() + theme(legend.title = element_blank(), legend.position = c(0.9, 0.75))
-        
-        p4 <- grid.arrange(p1, p2, p3, nrow = 3, ncol = 1)
-    })
 }
 
 shinyApp(ui, server)
