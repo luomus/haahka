ad71122dbc37917e40ecd210a5f94fff1304b19b
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-02 16:34:14 +0200

    Add new script for downloading metadata

---
+++ R/04_download_metadata.R
@@ -0,0 +1,11 @@
+library(googledrive)
+library(tidyverse)
+
+# FIXME: Add instructions on how to authorize with Google Drive
+
+# Download metadata spreadsheet from Google Drive and save as a CSV file
+dst_csv <- "data/text_and_image_reference.csv"
+
+dl <- googledrive::drive_download(
+  as_id("1pkJtIYh_YL91hMS7QdWE7Di95XHY2n3q_NzxrUICbz0"), path = dst_csv, 
+  overwrite = TRUE)
