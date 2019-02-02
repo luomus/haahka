027df687ce299e058dd8f7b495a53100306f99e7
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-02 16:10:28 +0200

    Add script file for downloading descriptions

---
+++ R/03_download_descriptions.R
@@ -0,0 +1,47 @@
+library(googledrive)
+library(tidyverse)
+
+# FIXME: Add instructions on how to authorize with Google Drive
+
+# Helper functions --------------------------------------------------------
+
+# Download a file from Google Drive
+#
+# @param x dribble row
+# @param path character string local path where to save the file
+#
+download_file <- function(x, path) {
+  
+  if (!file.exists(path)) {
+    dir.create(path)
+  }
+  # Build target file name. The format is XXX-YYYYYY.ext, where XXX are three 
+  # digits, YYYYYY is the species abbreviation and ext should be jpg, though 
+  # few pngs are there as well. YYYYYY should be in upper, but is delivered 
+  # in lower as well. Make it all upper.
+  body <- unlist(strsplit(x$name, "\\."))[1]
+  ext <- unlist(strsplit(x$name, "\\."))[2]
+  path <- file.path(path, paste0(toupper(body), ".", tolower(ext)))
+  
+  # Download file
+  googledrive::drive_download(googledrive::as_id(x$id), path = path, 
+                              overwrite = TRUE)
+  return(invisible(NULL))
+}
+
+# Process Drive content ---------------------------------------------------
+
+# Set the URL
+"https://drive.google.com/drive/folders/1wNUn4PY47jr5b77QkOMmjI7-fwIcFv2i" %>% 
+  # Transform URL to id
+  googledrive::as_id() %>% 
+  # Get Drive directory object
+  googledrive::drive_get() %>% 
+  # List photo files in the directory
+  googledrive::drive_ls() %>% 
+  # Process each row (i.e. file) in the dribble rowwise
+  dplyr::rowwise() %>% 
+  # Download files
+  dplyr::do(download_file(., path = "data/descriptions/test"))
+
+            
