0ee5cc5a9b31b503b9528d3b3ec78b7af15c3e6f
wkmor1 <willi@mmorris.email>
2023-07-28 17:05:47 +0300

    Update update script

--- update.R
+++ update.R
@@ -1,4 +1,25 @@
-source("R/01_download_photos.R")
-source("R/02_resize_photos.R")
-source("R/03_download_descriptions.R")
-source("R/04_download_data.R")
+res <- tryCatch(
+  {
+
+    source("download_photos.R")
+    source("resize_photos.R")
+    source("download_descriptions.R")
+    source("download_data.R")
+
+    message(sprintf("INFO [%s] Job complete", Sys.time()))
+
+    "true"
+
+  },
+  error = function(e) {
+
+    message(sprintf("ERROR [%s] %s", Sys.time(), e$message))
+
+    "false"
+
+  }
+)
+
+cat(res, file = "var/status/success.txt")
+
+cat(format(Sys.time(), usetz = TRUE), file = "var/status/last-update.txt")
