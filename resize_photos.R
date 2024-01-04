f50844df4d824b2bb04eceb95836d8a6afa3fba8
wkmor1 <willi@mmorris.email>
2024-01-04 15:51:49 +0200

    Refactor resize_photos.R

--- resize_photos.R
+++ resize_photos.R
@@ -5,42 +5,42 @@ library(progress, warn.conflicts = FALSE, quietly = TRUE)
 library(purrr, warn.conflicts = FALSE, quietly = TRUE)
 library(utils, warn.conflicts = FALSE, quietly = TRUE)
 
+resize_photo <- function(x, path = ".", width = 900) {
+
+  assertthat::assert_that(
+    width > 0, msg = "Width must be positive integer"
+  )
+
+  assertthat::validate_that(
+    width < 2000, msg = "Rescaling to a large value, probably not a good idea"
+  )
+
+  pb[["tick"]]()
+
+  if (path != "." && !file.exists(path)) {
+
+    dir.create(path)
+
+  }
+
+  target_file <- file.path(path, basename(x))
+
+  x <- magick::image_read(x)
+  x <- magick::image_scale(x, as.character(width))
+  x <- magick::image_write(x, path = target_file, format = "jpg")
+
+  invisible(NULL)
+
+}
+
 resize_all <- function(src_path, dst_path) {
 
   photo_files <- list.files(src_path, full.names = TRUE)
 
-  pb <- progress::progress_bar$new(
+  pb <- progress::progress_bar[["new"]](
     format = "  resizing [:bar] :percent in :elapsed",
     total = length(photo_files), clear = FALSE, width = 60
   )
-
-  resize_photo <- function(x, path = ".", width = 900) {
-
-    assertthat::assert_that(
-      width > 0, msg = "Width must be positive integer"
-    )
-
-    assertthat::validate_that(
-      width < 2000, msg = "Rescaling to a large value, probably not a good idea"
-    )
-
-    pb$tick()
-
-    if (path != "." && !file.exists(path)) {
-
-      dir.create(path)
-
-    }
-
-    target_file <- file.path(path, basename(x))
-
-    magick::image_read(x) %>%
-      magick::image_scale(as.character(width)) %>%
-      magick::image_write(path = target_file, format = "jpg")
-
-    invisible(NULL)
-
-  }
 
   purrr::walk(photo_files, resize_photo, path = dst_path)
 
