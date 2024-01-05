6cc188d6fd1c4c9ea7b2c7c25929facb81774304
wkmor1 <willi@mmorris.email>
2024-01-05 12:42:29 +0200

    Remove dependency on progress

--- resize_photos.R
+++ resize_photos.R
@@ -1,7 +1,6 @@
 library(assertthat, warn.conflicts = FALSE, quietly = TRUE)
 library(here, warn.conflicts = FALSE, quietly = TRUE)
 library(magick, warn.conflicts = FALSE, quietly = TRUE)
-library(progress, warn.conflicts = FALSE, quietly = TRUE)
 library(utils, warn.conflicts = FALSE, quietly = TRUE)
 
 resize_photo <- function(x, path = ".", width = 900, pb) {
@@ -13,8 +12,6 @@ resize_photo <- function(x, path = ".", width = 900, pb) {
   assertthat::validate_that(
     width < 2000, msg = "Rescaling to a large value, probably not a good idea"
   )
-
-  pb[["tick"]]()
 
   if (path != "." && !file.exists(path)) {
 
@@ -36,14 +33,9 @@ resize_all <- function(src_path, dst_path) {
 
   photo_files <- list.files(src_path, full.names = TRUE)
 
-  pb <- progress::progress_bar[["new"]](
-    format = "  resizing [:bar] :percent in :elapsed",
-    total = length(photo_files), clear = FALSE, width = 60
-  )
-
   for (f in photo_files) {
 
-    resize_photo(f, path = dst_path, pb = pb)
+    resize_photo(f, path = dst_path)
 
   }
 
