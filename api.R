c68d248e1eb6cef9e1c4a3fa45df90ca8f948727
wkmor1 <willi@mmorris.email>
2024-01-16 17:12:25 +0200

    Add graphics to API

--- api.R
+++ api.R
@@ -2,16 +2,24 @@
 #* @apiTOS https://laji.fi/en/about/845
 #* @apiContact list(name = "laji.fi support", email = "helpdesk@laji.fi")
 #* @apiLicense list(name = "MIT", url = "https://opensource.org/licenses/MIT")
+#* @apiTag graphics Get graphics
 
 suppressPackageStartupMessages({
 
+  library(dplyr, warn.conflicts = FALSE, quietly = TRUE)
   library(callr, warn.conflicts = FALSE, quietly = TRUE)
-  library(utils, warn.conflicts = FALSE, quietly = TRUE)
+  library(ggplot2, warn.conflicts = FALSE, quietly = TRUE)
+  library(haahka, warn.conflicts = FALSE, quietly = TRUE)
+  library(pool, warn.conflicts = FALSE, quietly = TRUE)
   library(rapidoc, warn.conflicts = FALSE, quietly = TRUE)
+  library(RPostgres, warn.conflicts = FALSE, quietly = TRUE)
+  library(utils, warn.conflicts = FALSE, quietly = TRUE)
 
 })
 
 version <- as.character(utils::packageVersion("haahka"))
+
+con <- pool::dbPool(RPostgres::Postgres(), dbname = Sys.getenv("DB_NAME"))
 
 #* @filter secret
 function(req, res) {
@@ -53,6 +61,77 @@ function() {
 #* @serializer unboxedJSON
 function() {
   ""
+}
+
+#* Get graphics for a species
+#* @tag graphics
+#* @get /plot/<type:str>/<sp:str>
+#* @param type:str Migration or Local
+#* @param sp:str Taxon code
+#* @serializer png list(width = 1200, height = 600, unit = "px", bg = "transparent")
+#* @response 200 A png file response
+function(type, sp) {
+
+  data <- dplyr::tbl(con, paste0(sp, "_data"))
+
+  data <- dplyr::collect(ans)
+
+  type <- tolower(type)
+
+  type_label <- switch(type, migration = "migrating", local = "stationary")
+
+  type <- switch(type, migration = "muutto", local = "paik")
+
+  data <- haahka::tile_observations(data, type)
+
+  ggplot2::ggplot() +
+    ggplot2::geom_rect(
+      ggplot2::aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+    data.frame(
+      xmin = seq(as.Date("2000-02-01"), by = "2 months", length.out = 7),
+      xmax = seq(as.Date("2000-03-01"), by = "2 months", length.out = 7),
+      ymin = 0,
+      ymax = Inf
+    ),
+    fill = "#f0f0f566"
+  ) +
+  ggplot2::geom_line(
+    ggplot2::aes(day, value), data, lwd = .5, col = "#1f78b4"
+  ) +
+  ggplot2::scale_x_date(
+    breaks = seq(as.Date("2000-01-01"), by = "month", length.out = 12),
+    date_labels = "%b",
+    limits = as.Date(c("2000-01-01", "2000-12-31")),
+    expand = c(0, 0)
+  ) +
+  ggplot2::scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +
+  ggplot2::xlab(NULL) +
+  ggplot2::ylab("Individuals/obs. day") +
+  ggplot2::ggtitle(sprintf("Average number of %s birds", type_label)) +
+  ggplot2::theme_gray(base_size = 6) +
+  ggplot2::theme(
+    plot.title = ggplot2::element_text(
+      hjust = 0.5, colour = "#333333", size = 9
+    ),
+    axis.text.x = ggplot2::element_text(colour = "#666666"),
+    axis.line.x = ggplot2::element_line(colour = "#ccd6eb", linewidth = .25),
+    axis.ticks.x = ggplot2::element_line(colour = "#ccd6eb", linewidth = .25),
+    axis.ticks.length.x = ggplot2::unit(5, "pt"),
+    axis.title.y = ggplot2::element_text(colour = "#666666"),
+    axis.ticks.y = ggplot2::element_blank(),
+    axis.text.y = ggplot2::element_text(
+      margin = ggplot2::margin(0, 5, 0, 0), colour = "#666666"
+    ),
+    panel.background = ggplot2::element_rect(fill = "transparent"),
+    panel.grid.major.x = ggplot2::element_blank(),
+    panel.grid.minor.x = ggplot2::element_blank(),
+    panel.grid.major.y = ggplot2::element_line(
+      colour = "#e6e6e6", linewidth = .25
+    ),
+    panel.grid.minor.y = ggplot2::element_blank(),
+    plot.background = ggplot2::element_rect(fill = "transparent", color = NA)
+  )
+
 }
 
 #* Get API spec
